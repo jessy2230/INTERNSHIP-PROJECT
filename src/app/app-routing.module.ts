72ac79b4566ffc09d33514b18de73baf0f163024
Jessica John <jessica.j@kristujayanti.com>
2024-06-06 22:10:14 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './component/profile/profile.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'profile',component:ProfileComponent
+  },
+  {
+    path:'home',component:HomePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { ProfileComponent } from './component/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
